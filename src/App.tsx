32347159e74dca2fe8f55f0a073634278c6fddb3
Marcos dev <marcosagusto1041@gmail.com>
2024-10-22 12:55:47 -0300

    fix: add GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { light } from './styles/theme';
 import { ThemeProvider } from 'styled-components';
 import MainRoutes from './routes/Routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <ThemeProvider theme={light}>
+        <GlobalStyle />
         <MainRoutes/>
     </ThemeProvider>
   )
